c3d078cc98bf8558fa615bcc34c4a422b506bc7a
allychao <73146562+allychao@users.noreply.github.com>
2021-09-30 04:46:42 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,6 +18,64 @@ let svg1 = d3.select('#vis1')
   .style('background-color', '#ccc') // change the background color to light gray
   .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
 
+d3.csv(
+  "https://raw.githubusercontent.com/DS4200-Fall21-Sec02/hw-03-practice-d3-hw-03-practice-d3-vn-ac/main/data/Boston%20Food%20Review%20-%20Sheet1.csv"
+).then(function (data) {
+  // create the subgroups using the 4th and 5th columns of the data (rating and distance)
+  let subgroups = data.columns.slice(4);
+
+  // create the list of groups
+  let groups = data.map((d) => d.group);
+
+  // add the created groups to the visualization
+  console.log(groups);
+
+  // add x axis
+  const x = d3.scaleBand().domain(groups).range([0, width]).padding([0.2]);
+  svg1
+    .append("g")
+    .attr("transform", "translate(0, ${height})")
+    .call(d3.axisBottom(x).tickSize(0));
+
+  // add y axis
+  const y = d3.scaleLinear().domain([0, 12]).range([height, 0]);
+  svg1.append("g").call(d3.axisLeft(y));
+
+  // scale subgroups
+  const xSubgroup = d3
+    .scaleBand()
+    .domain(subgroups)
+    .range([0, x.bandwidth()])
+    .padding([0.05]);
+
+  // create colors and give a color to each subgroup
+  const color = d3
+    .scaleOrdinal()
+    .domain(subgroups)
+    .range(["#0000ff", "#800080"]);
+
+  // show the bars
+  svg1
+    .append("g")
+    .selectAll("g")
+    // Enter in data
+    .data(data)
+    .join("g")
+    .attr("transform", (d) => `translate(${x(d.group)}, 0)`)
+    .selectAll("rect")
+    .data(function (d) {
+      return subgroups.map(function (key) {
+        return { key: key, value: d[key] };
+      });
+    })
+    .join("rect")
+    .attr("x", (d) => xSubgroup(d.key))
+    .attr("y", (d) => y(d.value))
+    .attr("width", xSubgroup.bandwidth())
+    .attr("height", (d) => height - y(d.value))
+    .attr("fill", (d) => color(d.key));
+});
+
 // second visualization
 let svg2 = d3.select('#vis2')
   .append('svg')
